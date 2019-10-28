2b465808a7d42464361b8f293358bf8082c564df
ivan.lazarev <ivan.lazarev@jadexconsulting.com>
2019-10-28 20:33:01 +0200

    express router

---
+++ routes/bootcamps.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+
+    const comments = {
+        // res.send({ name: 'Brad' });
+
+        // res.json({ success: false });
+        // res.sendStatus(400);
+    };
+});
+
+router.get('/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+router.post('/', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+});
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
+module.exports = router;
